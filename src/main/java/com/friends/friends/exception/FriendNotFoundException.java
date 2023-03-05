b9e31d98589f54c60659a64b20bb29704470767b
billmazio <bmaziotis@windowslive.com>
2023-03-05 17:00:47 +0200

    FriendNotFoundException.java

---
+++ src/main/java/com/friends/friends/exception/FriendNotFoundException.java
@@ -0,0 +1,7 @@
+package com.friends.friends.exception;
+
+public class FriendNotFoundException extends Exception{
+    public FriendNotFoundException(String message) {
+        super(message);
+    }
+}
