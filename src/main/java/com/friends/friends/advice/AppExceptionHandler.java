1720f7ebabf254d2f44537e80cdbaac354771e69
billmazio <bmaziotis@windowslive.com>
2023-03-05 17:02:10 +0200

    AppExceptionHandler.java

---
+++ src/main/java/com/friends/friends/advice/AppExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.friends.friends.advice;
+
+import com.friends.friends.exception.FriendNotFoundException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class AppExceptionHandler {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleInvalidArgument(MethodArgumentNotValidException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error->{
+            errorMap.put(error.getField(),error.getDefaultMessage());
+        });
+        return errorMap;
+    }
+
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(FriendNotFoundException.class)
+    public Map<String,String> handleBusinessException(FriendNotFoundException e) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("errorMessage", e.getMessage());
+        return errorMap;
+
+    }
+}
