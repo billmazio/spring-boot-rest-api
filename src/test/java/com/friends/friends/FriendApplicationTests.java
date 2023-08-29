1964d602c61529494b76f5aa126425bb87273da1
billmazio <bmaziotis@windowslive.com>
2023-08-29 22:36:17 +0300

    public Friend updateFriend(FriendRequest friendRequest , int id) throws FriendNotFoundException{         Friend friend = repository.findByFriendId(id);         friend.setFirstName(friendRequest.getFirstName());         friend.setLastName(friendRequest.getLastName());         friend.setEmail(friendRequest.getEmail());         friend.setMobile(friendRequest.getMobile());         if(friend != null) {             return friend;
            }else {
                throw new FriendNotFoundException("FRIEND NOT FOUND WITH ID:" + id);
    
            }

---
+++ src/test/java/com/friends/friends/FriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friends.friends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
